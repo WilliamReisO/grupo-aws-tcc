46e6ce46f5dffa2c10a2a737ae4dc5bfa2265dd1
WilliamReisO <oliveira._r@hotmail.com>
2023-06-28 20:56:33 -0300

    correção de rota

--- src/components/Paginas/curriculos/Curriculo.tsx
+++ src/components/Paginas/curriculos/Curriculo.tsx
@@ -21,17 +21,17 @@ import "swiper/css/pagination";
         className="mySwiper swiper1"
       >
         <SwiperSlide ><a href="https://drive.google.com/file/d/1ugB8ers6lKMJO07QYhWGijGWZAMmYiVt/view?usp=sharing"
-        target="_blank" rel="noopener noreferrer"><img src="public/img/Curriculo Thiago_page-0001.jpg"/></a>
+        target="_blank" rel="noopener noreferrer"><img src="img/Curriculo Thiago_page-0001.jpg"/></a>
         </SwiperSlide>
 
-        <SwiperSlide ><a href="https://drive.google.com/file/d/1mb8WETAf4yg9VjZtKYNz4yblBBF9RlRO/view?usp=sharing" target="_blank" rel="noopener noreferrer"><img src="public/img/Currículo Vitor Santos_page-0001.jpg" alt="vitor" /></a>
+        <SwiperSlide ><a href="https://drive.google.com/file/d/1mb8WETAf4yg9VjZtKYNz4yblBBF9RlRO/view?usp=sharing" target="_blank" rel="noopener noreferrer"><img src="img/Currículo Vitor Santos_page-0001.jpg" alt="vitor" /></a>
         </SwiperSlide>
-        <SwiperSlide ><a href="https://drive.google.com/file/d/1EfH4dod9gKqDPoWy4DsfIAwCYFKD84pL/view?usp=sharing" target="_blank" rel="noopener noreferrer"><img src="public/img/Currículo Wládia Patrícia_page-0001.jpg" alt="patricia"/></a>
+        <SwiperSlide ><a href="https://drive.google.com/file/d/1EfH4dod9gKqDPoWy4DsfIAwCYFKD84pL/view?usp=sharing" target="_blank" rel="noopener noreferrer"><img src="img/Currículo Wládia Patrícia_page-0001.jpg" alt="patricia"/></a>
         </SwiperSlide>
-        <SwiperSlide ><a href="https://drive.google.com/file/d/1RVyhR5gf0tCdkfk7e0UIMBB0Q4Nmh6Qe/view?usp=sharing" target="_blank" rel="noopener noreferrer"><img src="public/img/Curriculo William.png" alt="William"/></a>
+        <SwiperSlide ><a href="https://drive.google.com/file/d/1RVyhR5gf0tCdkfk7e0UIMBB0Q4Nmh6Qe/view?usp=sharing" target="_blank" rel="noopener noreferrer"><img src="img/Curriculo William.png" alt="William"/></a>
         </SwiperSlide>
         <SwiperSlide >Curriculo Vitor </SwiperSlide>
-        <SwiperSlide ><a href="https://drive.google.com/file/d/1-fBVXwcjuu2DZpIvlQ55K2R53sLCIdVy/view?usp=sharing" target="_blank" rel="noopener noreferrer"><img src="/public/img/Modelo-de-Currriculo.pdf.png" alt="patricia" width={150}/></a>
+        <SwiperSlide ><a href="https://drive.google.com/file/d/1-fBVXwcjuu2DZpIvlQ55K2R53sLCIdVy/view?usp=sharing" target="_blank" rel="noopener noreferrer"><img src="img/Modelo-de-Currriculo.pdf.png" alt="patricia" width={150}/></a>
         </SwiperSlide>
      </Swiper>
       </Grid>
